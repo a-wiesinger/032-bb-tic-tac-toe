779770fc5a055922fe63b9d4881591a32a27efbb
Aaron Wiesinger <aaron.wiesinger@gmail.com>
2022-11-01 07:18:02 -0400

    update to board setup

--- Models/Board.cs
+++ Models/Board.cs
@@ -3,7 +3,7 @@ namespace _032_bb_tic_tac_toe.Models;
 public class Board
 {
     // Properties
-    private char[,] GameBoard { get; set; } = new char[3, 3];
+    private char[][] GameBoard { get; set; } = new char[3][];
 
     public Board()
     {
@@ -12,20 +12,26 @@ public class Board
 
     public void DisplayGameBoard()
     {
-        for (int i = 0; i < GameBoard.Length; i++)
+        // Setup each row in the board
+        GameBoard[0] = new char[3];
+        GameBoard[1] = new char[3];
+        GameBoard[2] = new char[3];
+        Console.WriteLine("---------------------");
+        foreach (var item in GameBoard[0])
         {
-            if (i >= 0 && i < 3)
-            {
-                Console.WriteLine("Top line");
-            }
-            if (i >= 3 && i < 6)
-            {
-                Console.WriteLine("Middle Line");
-            }
-            if (i >= 6 && i < 9)
-            {
-                Console.WriteLine("Bottom Line");
-            }
+            Console.Write("| Top |");
         }
+        Console.WriteLine(); // Drop down
+        foreach (var item in GameBoard[1])
+        {
+            Console.Write("| Mid |");
+        }
+        Console.WriteLine(); // Drop down
+        foreach (var item in GameBoard[2])
+        {
+            Console.Write("| Bot |");
+        }
+        Console.WriteLine();
+        Console.WriteLine("---------------------");
     }
 }
